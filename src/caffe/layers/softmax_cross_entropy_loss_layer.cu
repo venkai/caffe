3bc64c967d2baf4c36065f0a133c09bf819dd472
Venkat Santhanam <venkai139@gmail.com>
2017-04-29 05:21:30 -0400

    SoftMaxCrossEntropyLoss: Improve GPU forward
    Implementation in https://github.com/richzhang/colorization
    for softmax_cross_entropy_loss_layer.cu performed the Forward Pass
    entirely in CPU. Forward pass is improved by computing the loss
    entirely in GPU.

--- src/caffe/layers/softmax_cross_entropy_loss_layer.cu
+++ src/caffe/layers/softmax_cross_entropy_loss_layer.cu
@@ -8,6 +8,20 @@
 namespace caffe {
 
 template <typename Dtype>
+__global__ void SoftmaxCrossEntropyLossForwardGPU(const int nthreads,
+    const Dtype* const target, const Dtype* const softmax_output,
+    Dtype* const loss_data) {
+  CUDA_KERNEL_LOOP(index, nthreads) {
+    if (target[index] > 0) {
+      loss_data[index] = target[index] *
+        (log(target[index]) - log(softmax_output[index] + Dtype(FLT_MIN)));
+    } else {
+      loss_data[index] = static_cast<Dtype>(0);
+    }
+  }
+}
+
+template <typename Dtype>
 void SoftmaxCrossEntropyLossLayer<Dtype>::Forward_gpu(
     const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
   // The forward pass computes the softmax outputs.
@@ -16,18 +30,20 @@ void SoftmaxCrossEntropyLossLayer<Dtype>::Forward_gpu(
   // Compute the loss (negative log likelihood)
   const int count = bottom[0]->count();
   const int num = bottom[0]->num();
-  // Stable version of loss computation from input data
-  const Dtype* input_data = bottom[0]->cpu_data();
-  const Dtype* target = bottom[1]->cpu_data();
+  // Since this memory is not used for anything until it is overwritten
+  // on the backward pass, we use it here to avoid having to allocate new GPU
+  // memory to accumulate intermediate results in the kernel.
+  Dtype* loss_data = bottom[0]->mutable_gpu_diff();
+  // Bottom activations
+  const Dtype* const target = bottom[1]->gpu_data();
   // Output of softmax forward pass
-  const Dtype* softmax_output = softmax_top_vec_[0]->cpu_data();
-  Dtype loss = 0;
-  for (int i = 0; i < count; ++i) {
-    if ( target[i] > 0 ) {
-        loss -= target[i] *
-            (log(softmax_output[i] + Dtype(FLT_MIN)) - log(target[i]));
-    }
-  }
+  const Dtype* const softmax_output = softmax_top_vec_[0]->gpu_data();
+  // Stable version of loss computation from input data
+  // NOLINT_NEXT_LINE(whitespace/operators)
+  SoftmaxCrossEntropyLossForwardGPU<Dtype><<<CAFFE_GET_BLOCKS(count),
+      CAFFE_CUDA_NUM_THREADS>>>(count, target, softmax_output, loss_data);
+  Dtype loss;
+  caffe_gpu_asum(count, loss_data, &loss);
   top[0]->mutable_cpu_data()[0] = loss / num;
 }
 
@@ -43,8 +59,8 @@ void SoftmaxCrossEntropyLossLayer<Dtype>::Backward_gpu(
     // First, compute the diff
     const int count = bottom[0]->count();
     const int num = bottom[0]->num();
-    const Dtype* softmax_output_data = softmax_output_->gpu_data();
-    const Dtype* target = bottom[1]->gpu_data();
+    const Dtype* const softmax_output_data = softmax_output_->gpu_data();
+    const Dtype* const target = bottom[1]->gpu_data();
     Dtype* bottom_diff = bottom[0]->mutable_gpu_diff();
     caffe_copy(count, softmax_output_data, bottom_diff);
     caffe_gpu_axpy(count, Dtype(-1), target, bottom_diff);
